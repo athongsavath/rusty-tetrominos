0f9c3dc90f9345b179b93f79d4c2a7b8328ae2ce
Andrew Thongsavath <andrew@athongsavath.com>
2020-12-29 22:46:54 -0600

    Implement random color

---
+++ src/color.rs
@@ -0,0 +1,21 @@
+use crossterm::style::Color;
+use rand::Rng;
+
+pub fn random_color() -> Color {
+    let mut rng = rand::thread_rng();
+    match rng.gen_range(0..=11) {
+        0 => Color::Red,
+        1 => Color::Green,
+        2 => Color::Yellow,
+        3 => Color::Blue,
+        4 => Color::Magenta,
+        5 => Color::Cyan,
+        6 => Color::DarkRed,
+        7 => Color::DarkGreen,
+        8 => Color::DarkYellow,
+        9 => Color::DarkBlue,
+        10 => Color::DarkMagenta,
+        11 => Color::DarkCyan,
+        _ => panic!("RNG went out of range for random color."),
+    }
+}
